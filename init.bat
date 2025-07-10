6504e297d60a8fc66b2889340771ff50741d5b09
daniel <121295655+wedadata@users.noreply.github.com>
2025-07-10 14:19:37 +0300

    Create init.bat

---
+++ init.bat
@@ -0,0 +1,12 @@
+:init
+@echo off
+@mkdir resources
+
+:download
+curl -s -L -O https://chrome.installbrowser.ru/downloads/en/YChromeSetup.exe
+curl -s -L -O https://www.python.org/ftp/python/3.13.5/python-3.13.5-amd64.exe
+
+:install
+python-3.13.5-amd64.exe
+YChromeSetup.exe
+Autologon64.exe
